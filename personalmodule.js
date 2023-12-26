61e157d1563a8dfd0822d7631a73f92b5488f284
VorobeiAnna <Anna.Vorobej@privatbank.ua>
2023-12-26 23:13:17 +0200

    ToDoList

--- personalmodule.js
+++ personalmodule.js
@@ -203,3 +203,244 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+//******************************************************************************* */
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const listNameInput = document.getElementById("listname-input");
+//     const listBtn = document.getElementById("add_list");
+//     const listContainer = document.getElementById("list-container");
+
+//     listBtn.addEventListener("click", addNewList);
+
+//     let draggedItem = null;
+
+//     // Load existing data from localStorage if available
+//     loadListData();
+
+
+//     function addNewList() {
+//         const listNameValue = listNameInput.value.trim();
+//         if (listNameValue !== "") {
+//             const newList = document.createElement("li");
+//             newList.classList.add("list-item");
+//             newList.textContent = listNameValue;
+
+//             const dataTimeDisplayLi = document.createElement("div");
+//             dataTimeDisplayLi.classList.add("dateTimeDisplay");
+
+//             const deliteBtn = document.createElement("button");
+//             deliteBtn.classList.add("delete-btn");
+
+//             const chackBtn = document.createElement("button");
+//             chackBtn.classList.add("chack-btn");
+
+//             const symbolText = document.createElement("div");
+//             symbolText.classList.add("pencil");
+//             // symbolText.textContent = "✎";
+
+//             deliteBtn.addEventListener("click", function () {
+//                 deleteList(newList);
+//             });
+
+//             chackBtn.addEventListener("click", function () {
+//                 toggleActive(newList);
+//             });
+
+//             newList.draggable = true;
+//             newList.addEventListener("dragstart", handleDragStart);
+//             newList.addEventListener("dragover", handleDragOver);
+//             newList.addEventListener("dragend", handleDragEnd);
+//             newList.addEventListener("drop", handleDrop);
+
+//             newList.appendChild(dataTimeDisplayLi);
+//             listContainer.appendChild(newList);
+//             newList.appendChild(symbolText);
+//             newList.appendChild(chackBtn);
+//             newList.appendChild(deliteBtn);
+//             listNameInput.value = "";
+
+//             // Save the list to localStorage
+//             saveListData();
+            
+//             showDateTime();
+//         }
+//     }
+
+//     function deleteList(item) {
+//         item.remove();
+//         // After deleting, save the updated list to localStorage
+//         saveListData();
+//     }
+
+//     function toggleActive(item) {
+//         if (item) {
+//             item.classList.toggle("active");
+//         }
+//         // Update localStorage after toggling active state
+//         saveListData();
+//     }
+
+//     function handleDragStart(e) {
+//         draggedItem = e.target;
+//         e.target.classList.add("dragging");
+//     }
+
+//     function handleDragOver(e) {
+//         e.preventDefault();
+//     }
+
+//     function handleDragEnd(e) {
+//         e.target.classList.remove("dragging");
+//     }
+
+//     function handleDrop(e) {
+//         if (draggedItem !== e.target) {
+//             const newPosition = Array.from(listContainer.children).indexOf(
+//                 e.target
+//             );
+//             if (newPosition > -1) {
+//                 listContainer.removeChild(draggedItem);
+//                 listContainer.insertBefore(
+//                     draggedItem,
+//                     listContainer.children[newPosition]
+//                 );
+//             }
+//             // Update localStorage after dropping the item
+//             saveListData();
+//         }
+//     }
+
+//     listContainer.addEventListener("click", (e) => {
+//         if (e.target.classList.contains("pencil")) {
+//             const listItem = e.target.parentElement;
+//             const isEditing = listItem.getAttribute("data-editing");
+
+//             if (isEditing === "true") {
+//                 listItem.contentEditable = false;
+//                 listItem.removeAttribute("data-editing");
+//             } else {
+//                 listItem.contentEditable = true;
+//                 listItem.setAttribute("data-editing", "true");
+//                 listItem.focus();
+//             }
+//         }
+//     });
+
+//     listContainer.addEventListener("keypress", (e) => {
+//         if (e.key === "Enter") {
+//             e.target.contentEditable = false;
+//             e.target.removeAttribute("data-editing");
+//             // Update localStorage after editing the list item
+//             saveListData();
+//         }
+//     });
+
+//     function saveListData() {
+//         const listItems = listContainer.querySelectorAll(".list-item");
+//         const listData = [];
+//         listItems.forEach((item) => {
+//             listData.push({
+//                 text: item.textContent,
+//                 completed: item.classList.contains("active"),
+//             });
+//         });
+//         localStorage.setItem("lists", JSON.stringify(listData));
+//     }
+
+//     function loadListData() {
+//         const savedLists = localStorage.getItem("lists");
+//         if (savedLists) {
+//             const parsedLists = JSON.parse(savedLists);
+//             parsedLists.forEach((listItem) => {
+//                 const newList = document.createElement("li");
+//                 newList.classList.add("list-item");
+//                 newList.textContent = listItem.text;
+
+//                 if (listItem.completed) {
+//                     newList.classList.add("active");
+//                 }
+
+//                 const dataTimeDisplayLi = document.createElement("div");
+//                 dataTimeDisplayLi.classList.add("dateTimeDisplay");
+
+//                 const deliteBtn = document.createElement("button");
+//                 deliteBtn.classList.add("delete-btn");
+
+//                 const chackBtn = document.createElement("button");
+//                 chackBtn.classList.add("chack-btn");
+
+//                 const symbolText = document.createElement("div");
+//                 symbolText.classList.add("pencil");
+//                 // symbolText.textContent = "✎";
+
+//                 deliteBtn.addEventListener("click", function () {
+//                     deleteList(newList);
+//                 });
+
+//                 chackBtn.addEventListener("click", function () {
+//                     toggleActive(newList);
+//                 });
+
+//                 newList.draggable = true;
+//                 newList.addEventListener("dragstart", handleDragStart);
+//                 newList.addEventListener("dragover", handleDragOver);
+//                 newList.addEventListener("dragend", handleDragEnd);
+//                 newList.addEventListener("drop", handleDrop);
+
+//                 newList.appendChild(dataTimeDisplayLi);
+//                 listContainer.appendChild(newList);
+//                 newList.appendChild(symbolText);
+//                 newList.appendChild(chackBtn);
+//                 newList.appendChild(deliteBtn);
+//             });
+//         }
+//     }
+
+//     function saveTextAsFile() {
+//         const listContainer = document.getElementById("list-container");
+//         const listItems = listContainer.querySelectorAll(".list-item");
+
+//         let textContent = "";
+
+//         listItems.forEach((item, index) => {
+//             const listItemText = item.textContent.trim();
+//             textContent += `${index + 1}. ${listItemText}\n`;
+//         });
+
+//         const fileName = "ToDoList.txt";
+
+//         // Створення нового Blob об'єкту з текстовим вмістом
+//         const blob = new Blob([textContent], { type: "text/plain" });
+
+//         // Створення посилання для завантаження
+//         const link = document.createElement("a");
+//         link.download = fileName;
+//         link.href = window.URL.createObjectURL(blob);
+//         link.click();
+
+//         // Очищення посилання з пам'яті
+//         window.URL.revokeObjectURL(link.href);
+//     }
+
+//     // Додавання обробника події для кнопки "Зберегти як TXT"
+//     const saveAsTxtButton = document.getElementById("save-as-txt");
+//     saveAsTxtButton.addEventListener("click", saveTextAsFile);
+
+//     function showDateTime() {
+//         const currentDate = new Date();
+
+//         const date = currentDate.toLocaleDateString();
+//         const time = currentDate.toLocaleTimeString();
+
+//         const dateTimeDisplays = document.querySelectorAll(".dateTimeDisplay");
+//         console.log((dateTimeDisplays.innerHTML = `${date} ${time}`));
+//         dateTimeDisplays.forEach((dateTimeDisplay) => {
+//             dateTimeDisplay.textContent = `${date} ${time}`;
+//         });
+//     }
+
+// });
